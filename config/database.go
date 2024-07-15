998fab934ce89c539edc9d5ba0678d38f9ab4e98
Prateek Kumar Singh <prateek.singh@interface.ai>
2024-07-15 22:20:46 +0530

    1st basic commit done

---
+++ config/database.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+    "github.com/pksingh21/pyqbackend/entities"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+var Database *gorm.DB
+
+func Connect() error {
+    var err error
+
+	err = godotenv.Load()
+	if err != nil {
+        panic(err)
+    }
+	var DATABASE_URI string = os.Getenv("POSTGRES_URI")
+    Database, err = gorm.Open(postgres.Open(DATABASE_URI), &gorm.Config{
+        SkipDefaultTransaction: true,
+        PrepareStmt:            true,
+    })
+
+    if err != nil {
+        panic(err)
+    }
+
+    Database.AutoMigrate(&entities.Dog{})
+
+    return nil
+}
