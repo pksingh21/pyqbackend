ffbdf7a3be70e0bfd97b7b5af051ca68bedb4caa
Prateek Kumar Singh <prateek.singh@interface.ai>
2024-07-16 19:24:55 +0530

    Added the auto migrate for all entities

--- config/database.go
+++ config/database.go
@@ -1,11 +1,11 @@
 package config
 
 import (
-    // "github.com/pksingh21/pyqbackend/entity"
+    "github.com/pksingh21/pyqbackend/entity"
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
-	"github.com/joho/godotenv"
-	"os"
+    "github.com/joho/godotenv"
+    "os"
 )
 
 var Database *gorm.DB
@@ -13,21 +13,51 @@ var Database *gorm.DB
 func Connect() error {
     var err error
 
-	err = godotenv.Load()
-	if err != nil {
-        panic(err)
-    }
-	var DATABASE_URI string = os.Getenv("POSTGRES_URI")
-    Database, err = gorm.Open(postgres.Open(DATABASE_URI), &gorm.Config{
-        SkipDefaultTransaction: true,
-        PrepareStmt:            true,
-    })
-
+    // Load environment variables
+    err = godotenv.Load()
     if err != nil {
         panic(err)
     }
 
-    // Database.AutoMigrate(&entities.Dog{})
+    // Get PostgreSQL URI from environment
+    DATABASE_URI := os.Getenv("POSTGRES_URI")
+
+    // Connect to the database
+    Database, err = gorm.Open(postgres.Open(DATABASE_URI), &gorm.Config{
+        SkipDefaultTransaction: true,
+        PrepareStmt:            true,
+    })
+    if err != nil {
+        panic(err)
+    }
+
+    // Auto migrate all entities
+    err = AutoMigrateEntities(Database)
+    if err != nil {
+        panic(err)
+    }
+
+    return nil
+}
+
+func AutoMigrateEntities(db *gorm.DB) error {
+    // List of all entities to migrate
+    entities := []interface{}{
+        &entities.User{},
+        &entities.Test{},
+        &entities.Option{},
+        &entities.Answer{},
+        &entities.Paper{},
+        &entities.Question{},
+        &entities.Tag{},
+    }
+
+    // Auto migrate each entity
+    for _, entity := range entities {
+        if err := db.AutoMigrate(entity); err != nil {
+            return err
+        }
+    }
 
     return nil
 }
